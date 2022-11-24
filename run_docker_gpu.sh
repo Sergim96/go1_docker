6d286e063ec860ed646d5c677a5389d3976511bb
Sergim96 <sergi.saya96@gmail.com>
2022-11-24 17:19:50 +0000

    gui for cpu

--- run_docker_gpu.sh
+++
@@ -1,7 +0,0 @@
-# Original command (broke)
-docker run -it --net=host --gpus all \
-    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    go1_dev \
